43e9d1171dec0bc5180eccbead86ee4d943a1a3d
Josh Hill <joshuahill44@gmail.com>
2013-08-13 11:22:10 -0400

    Clean up, start form over

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -29,4 +29,22 @@ module ApplicationHelper
     @current_user || User.find_by_session_token(session[:session_token])
   end
 
+
+  def link_to_add_fields(name, f, association, child_association = nil)
+    new_object = f.object.send(association).klass.new
+
+    if child_association
+      3.times{ new_object.send(child_association).build } #Builds 3 Answers
+      #new_object.send(child_association).build #Builds 1 Answer
+    end
+
+    id = new_object.object_id
+
+    fields = f.fields_for(association, new_object, child_index: id) do |builder|
+      render(association.to_s.singularize + "_fields", f: builder)
+    end
+
+    link_to(name, '#', class: "add_fields", data: {id: id, fields: fields.gsub("\n", "")})
+  end
+  
 end
