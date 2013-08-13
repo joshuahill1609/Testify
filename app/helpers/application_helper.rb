711a2de009cdb90fe22de9dabf7087646abccecb
Josh Hill <joshuahill44@gmail.com>
2013-08-13 11:46:47 -0400

    Fuck yeah!! Got the form working but lost styling

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -30,9 +30,13 @@ module ApplicationHelper
   end
 
 
-  def link_to_add_fields(name, f, association, child_association = nil)
+  def link_to_add_fields(name, f, association, correct_answer_child = nil, child_association = nil)
     new_object = f.object.send(association).klass.new
 
+    if correct_answer_child
+      1.times { new_object.send(correct_answer_child).build }
+    end
+    
     if child_association
       3.times{ new_object.send(child_association).build } #Builds 3 Answers
       #new_object.send(child_association).build #Builds 1 Answer
