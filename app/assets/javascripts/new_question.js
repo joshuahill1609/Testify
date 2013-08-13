f19832771fc2faddd684d88ef4ea5f0266f6539a
Josh Hill <joshuahill44@gmail.com>
2013-08-13 11:57:43 -0400

    Clean UP

--- app/assets/javascripts/new_question.js
+++ app/assets/javascripts/new_question.js
@@ -2,6 +2,8 @@ $(document).ready(function(){
 
 	$(".sortable").sortable();
 	
+	
+	// gives "save" button on exam build page functionality
 	$("#exam_save").click(function(){
 		$("#new_exam").submit();
 		return false;
