eb6547551d47d5140c3ea564c0b2e076f1c64663
Josh Hill <josh@vauthierferguson.com>
2013-09-06 13:00:14 -0600

    Fix after sign in route

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,8 @@ class ApplicationController < ActionController::Base
   protect_from_forgery
 
   include ApplicationHelper
+  
+  def after_sign_in_path_for(resource)
+    users_path
+  end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
 
   def index
-    @users = User.all
+    redirect_to "/users/#{current_user.id}"
   end
 
   def new
